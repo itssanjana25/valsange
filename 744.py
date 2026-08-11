5f2560e7237cda50e3b68c37358ccb9b9877f9ac
itssanjana25 <sanjanavalasange@gmail.com>
2026-08-11 15:50:18 +0530

    Update 744.py

--- 744.py
+++ 744.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def nextGreatestLetter(self, letters, target):
         low = 0
-        high = 0
+        high = len(letters) - 1
         while low <= high :
             mid = low + (high-low)/2 
+
             if letters[mid] <= target:
                 low = mid+1
             else:
-                high = mid+1
+                high = mid-1
                 
-        return letters[ow % letters -length]
+        return letters[low % len(letters)]
         
