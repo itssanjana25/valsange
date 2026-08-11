f06874087f7651e9e01bec82e02899eb4ab99ddd
itssanjana25 <sanjanavalasange@gmail.com>
2026-08-11 17:09:21 +0530

    Implement backspace string comparison solution

---
+++ 844.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        def build (string):
+            stack = deque()
+            for char in string:
+
+                if char != '#':
+                    stack.append(char)
+                elif stack:
+                    stack.pop()
+                    
+            return "".join(stack)
+        
+        return build(s)== build(t)
+        
+        
